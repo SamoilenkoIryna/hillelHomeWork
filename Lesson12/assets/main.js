5c1e8b7b7344cb1a394f2e95158f8f3c8c96e973
Iryna Samoilenko <yarishka007@gmail.com>
2021-01-22 14:01:41 +0200

    Lesson 12

---
+++ Lesson12/assets/main.js
@@ -0,0 +1,34 @@
+const calcSum = function() {
+    let result = 0;
+    
+    return function(num){
+
+        return result = result + num;
+    };
+};
+const calc = calcSum();
+
+
+const createCounter = function(start, step) {
+    let counter = start;
+   
+        return function(reset) { 
+            if (reset === 'resetCounter') {
+                counter = start;
+            }
+
+            return counter = counter + step;
+        };
+    
+};
+
+const fbLikes = createCounter(0, 1);
+
+console.log('fb: ',fbLikes());
+console.log('fb: ',fbLikes());
+console.log('fb: ',fbLikes());
+console.log('fb: ',fbLikes('resetCounter'));
+console.log('fb: ',fbLikes());
+console.log('fb: ',fbLikes());
+console.log('fb: ',fbLikes());
+console.log('fb: ',fbLikes());
